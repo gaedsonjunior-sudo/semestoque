d88f1533176a25b7c3d750bb1aca3669259f605c
gaedsonjunior-sudo <ga.edson.junior@gmail.com>
2026-01-26 14:45:47 -0300

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -5,12 +5,13 @@
 // IMPORTANTE: Substitua estas configurações pelas suas do Firebase Console
 // Veja o arquivo GUIA_FIREBASE_PASSO_A_PASSO.md para instruções detalhadas
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto-id",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "seu-app-id"
+  apiKey: "AIzaSyBwu6xJDPPX33_Qs-ULFo2hFZ2wDgJ6ywg",
+  authDomain: "semestoque-f3a2a.firebaseapp.com",
+  projectId: "semestoque-f3a2a",
+  storageBucket: "semestoque-f3a2a.firebasestorage.app",
+  messagingSenderId: "12189603066",
+  appId: "1:12189603066:web:13e1cfce4d02363d342543",
+  measurementId: "G-QJE8EWZ9NB"
 };
 
 // Verifica se o Firebase está disponível
